e7a04062893205296fcf353be53d44b5cc5da3fb
Patrick <psharp@greenspice.com>
2021-06-02 17:14:05 -0700

    Get japanese vs Hindi binary classifier to work

---
+++ accuracy_records.py
@@ -0,0 +1,5 @@
+# Japanese and Hindi
+error_rates = [0.029586, 0.059172, 0.065476, 0.017857, 0.011905, 0.029762, 0.059524, 0.029762, 0.053892, 0.017964]
+accuracy = 1 - sum(error_rates) / 10
+print(f"Overall accuracy is {accuracy * 100}%")
+# Overall accuracy is 96.251%
